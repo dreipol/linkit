37f0a836d3d948206026fa25144cc5ec65b5469a
Sidney Widmer <sidneywidmer@gmail.com>
2019-07-24 13:17:51 +0200

    Add `set` property on Link
    This allows us to easily check if an optional LinkField
    has a set value or not.

--- linkit/link.py
+++ linkit/link.py
@@ -78,3 +78,23 @@ class Link(object):
     def config_to_json(self) -> str:
         """ Only used for debugging in the template. """
         return json.dumps(self.config())
+
+    @property
+    def set(self) -> bool:
+        """
+        Check if we have a value. If we have and its a dict, check if any of the properties
+        actually contain anything. We explicitly don't use __bool__ since it's not the
+        right use case for this scenario.
+        """
+        value = self.data('value', None)
+        if not value:
+            return False
+
+        has_data = False
+        if isinstance(value, dict):
+            for key, v in value.items():
+                if v:
+                    has_data = True
+                    break
+
+        return has_data
